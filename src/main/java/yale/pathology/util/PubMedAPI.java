decfe59bfcca8ef210fcc4ec25668c6562c893f3
syberian <peter.gershkovich@gmail.com>
2015-01-18 11:38:36 -0500

    Initial commit

---
+++ src/main/java/yale/pathology/util/PubMedAPI.java
@@ -0,0 +1,15 @@
+package yale.pathology.util;
+
+/**
+ * Created by petergershkovich on 1/18/15.
+ */
+public class PubMedAPI
+{
+    public static void main(String[] args)
+    {
+        //get pubmed data from Europe PubMed
+
+
+
+    }
+}
