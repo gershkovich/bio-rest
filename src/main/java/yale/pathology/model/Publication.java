f89af89033186a98283a7b9ea519dd7fd7b795a5
Peter Gershkovich <peter.gershkovich@yale.edu>
2015-01-19 09:33:10 -0500

    publication mapping

---
+++ src/main/java/yale/pathology/model/Publication.java
@@ -0,0 +1,348 @@
+package yale.pathology.model;
+
+/**
+ * Created by pg86 on 1/19/15.
+ */
+public class Publication
+{
+    //{"id":"23372985",
+    // "source":"MED",
+    // "pmid":"23372985",
+    // "pmcid":"PMC3551490",
+    // "title":"Custom software development for use in a clinical laboratory.",
+    // "authorString":"Sinard JH, Gershkovich P.",
+    // "journalTitle":"J Pathol Inform",
+    // "journalVolume":"3",
+    // "pubYear":"2012",
+
+    // "journalIssn":"2229-5089",
+    // "pageInfo":"44",
+    // "pubType":"journal article",
+    // "isOpenAccess":"Y",
+    // "inEPMC":"Y",
+    // "inPMC":"Y",
+    // "citedByCount":2,
+    // "hasReferences":"N",
+    // "hasTextMinedTerms":"Y",
+    // "hasDbCrossReferences":"N",
+    // "hasLabsLinks":"N",
+    // "hasTMAccessionNumbers":"N",
+    // "luceneScore":"1186.1707",
+    // "doi":"10.4103/2153-3539.104906"}
+
+
+    private String id;
+    private String source;
+
+    private String pmid;
+    private String pmcid;
+    private String title;
+    private String authorString;
+    private String journalTitle;
+    private String journalVolume;
+    private String pubYear;
+    private String journalIssn;
+    private String pageInfo;
+    private String pubType;
+    private String isOpenAccess;
+    private String inEPMC;
+    private String inPMC;
+    private String citedByCount;
+    private String hasReferences;
+    private String hasTextMinedTerms;
+    private String hasDbCrossReferences;
+    private String hasLabsLinks;
+    private String hasTMAccessionNumbers;
+    private String luceneScore;
+    private String doi;
+
+
+    public String getId()
+    {
+
+        return id;
+    }
+
+    public void setId(String id)
+    {
+
+        this.id = id;
+    }
+
+    public String getSource()
+    {
+
+        return source;
+    }
+
+    public void setSource(String source)
+    {
+
+        this.source = source;
+    }
+
+    public String getPmid()
+    {
+
+        return pmid;
+    }
+
+    public void setPmid(String pmid)
+    {
+
+        this.pmid = pmid;
+    }
+
+    public String getPmcid()
+    {
+
+        return pmcid;
+    }
+
+    public void setPmcid(String pmcid)
+    {
+
+        this.pmcid = pmcid;
+    }
+
+    public String getTitle()
+    {
+
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+
+        this.title = title;
+    }
+
+    public String getAuthorString()
+    {
+
+        return authorString;
+    }
+
+    public void setAuthorString(String authorString)
+    {
+
+        this.authorString = authorString;
+    }
+
+    public String getJournalTitle()
+    {
+
+        return journalTitle;
+    }
+
+    public void setJournalTitle(String journalTitle)
+    {
+
+        this.journalTitle = journalTitle;
+    }
+
+    public String getJournalVolume()
+    {
+
+        return journalVolume;
+    }
+
+    public void setJournalVolume(String journalVolume)
+    {
+
+        this.journalVolume = journalVolume;
+    }
+
+    public String getPubYear()
+    {
+
+        return pubYear;
+    }
+
+    public void setPubYear(String pubYear)
+    {
+
+        this.pubYear = pubYear;
+    }
+
+    public String getJournalIssn()
+    {
+
+        return journalIssn;
+    }
+
+    public void setJournalIssn(String journalIssn)
+    {
+
+        this.journalIssn = journalIssn;
+    }
+
+    public String getPageInfo()
+    {
+
+        return pageInfo;
+    }
+
+    public void setPageInfo(String pageInfo)
+    {
+
+        this.pageInfo = pageInfo;
+    }
+
+    public String getPubType()
+    {
+
+        return pubType;
+    }
+
+    public void setPubType(String pubType)
+    {
+
+        this.pubType = pubType;
+    }
+
+    public String getIsOpenAccess()
+    {
+
+        return isOpenAccess;
+    }
+
+    public void setIsOpenAccess(String isOpenAccess)
+    {
+
+        this.isOpenAccess = isOpenAccess;
+    }
+
+    public String getInEPMC()
+    {
+
+        return inEPMC;
+    }
+
+    public void setInEPMC(String inEPMC)
+    {
+
+        this.inEPMC = inEPMC;
+    }
+
+    public String getInPMC()
+    {
+
+        return inPMC;
+    }
+
+    public void setInPMC(String inPMC)
+    {
+
+        this.inPMC = inPMC;
+    }
+
+    public String getCitedByCount()
+    {
+
+        return citedByCount;
+    }
+
+    public void setCitedByCount(String citedByCount)
+    {
+
+        this.citedByCount = citedByCount;
+    }
+
+    public String getHasReferences()
+    {
+
+        return hasReferences;
+    }
+
+    public void setHasReferences(String hasReferences)
+    {
+
+        this.hasReferences = hasReferences;
+    }
+
+    public String getHasTextMinedTerms()
+    {
+
+        return hasTextMinedTerms;
+    }
+
+    public void setHasTextMinedTerms(String hasTextMinedTerms)
+    {
+
+        this.hasTextMinedTerms = hasTextMinedTerms;
+    }
+
+    public String getHasDbCrossReferences()
+    {
+
+        return hasDbCrossReferences;
+    }
+
+    public void setHasDbCrossReferences(String hasDbCrossReferences)
+    {
+
+        this.hasDbCrossReferences = hasDbCrossReferences;
+    }
+
+    public String getHasLabsLinks()
+    {
+
+        return hasLabsLinks;
+    }
+
+    public void setHasLabsLinks(String hasLabsLinks)
+    {
+
+        this.hasLabsLinks = hasLabsLinks;
+    }
+
+    public String getHasTMAccessionNumbers()
+    {
+
+        return hasTMAccessionNumbers;
+    }
+
+    public void setHasTMAccessionNumbers(String hasTMAccessionNumbers)
+    {
+
+        this.hasTMAccessionNumbers = hasTMAccessionNumbers;
+    }
+
+    public String getLuceneScore()
+    {
+
+        return luceneScore;
+    }
+
+    public void setLuceneScore(String luceneScore)
+    {
+
+        this.luceneScore = luceneScore;
+    }
+
+    public String getDoi()
+    {
+
+        return doi;
+    }
+
+    public void setDoi(String doi)
+    {
+
+        this.doi = doi;
+    }
+
+    /**
+     * Example
+     * [1] Sinard JH, Gershkovich P: Custom software development for use in a clinical laboratory. Journal of pathology informatics 2012, 3:44.
+     * Sinard JH, Gershkovich P.: Custom software development for use in a clinical laboratory. J Pathol Inform 2012, 3:44
+     * @return
+     */
+
+    public String toString()
+    {
+        return String.format(" %s: %s %s %s, %s:%s (%s)", getAuthorString(),  getTitle(), getJournalTitle(), getPubYear(), getJournalVolume(), getPageInfo(), getPmcid());
+
+    }
+}
--- src/test/yale/pathology/util/PubMedAPITest.java
+++ src/test/yale/pathology/util/PubMedAPITest.java
@@ -3,9 +3,16 @@ package yale.pathology.util;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jdk.nashorn.internal.parser.JSONParser;
 import org.junit.Before;
 import org.junit.Test;
+import yale.pathology.model.Publication;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import static org.junit.Assert.*;
 
@@ -37,12 +44,16 @@ public class PubMedAPITest
         //demonstrates concatenated list of search elements - looks far from standards!
         String authAndJournal = "http://www.ebi.ac.uk/europepmc/webservices/rest/search/query=auth:%22Gershkovich%20P%22%20JOURNAL:%22Proc%20AMIA%20Symp%22&format=json";
 
-        String auth = "http://www.ebi.ac.uk/europepmc/webservices/rest/search/query=auth:Gershkovich&format=json";
+       String auth = "http://www.ebi.ac.uk/europepmc/webservices/rest/search/query=auth:%22Gershkovich%20P%22%20&format=json";
 
         System.out.println(pubMedAPI.getResource(auth)); //returns too many results
 
     }
 
+    /**
+     * Tokenize request
+     * @throws Exception
+     */
     @Test
     public void citationQuery() throws Exception
     {
@@ -92,5 +103,67 @@ public class PubMedAPITest
         assertTrue(results.contains("Gershkovich"));
 
     }
+
+
+    /**
+     * Parse to object
+     * @throws Exception
+     */
+    @Test
+    public void parseAuthorQuery() throws Exception
+    {
+        PubMedAPI pubMedAPI = new PubMedAPI();
+
+        //demonstrates concatenated list of search elements - looks far from standards!
+        String pubmedId = "http://www.ebi.ac.uk/europepmc/webservices/rest/search/query=ext_id:24926086&format=json";
+
+        String auth = "http://www.ebi.ac.uk/europepmc/webservices/rest/search/query=auth:%22Gershkovich%20P%22%20&format=json";
+
+        String results = pubMedAPI.getResource(auth);
+
+        //   System.out.println(results); //returns too many results
+
+        ArrayList<Publication> publications = new ArrayList<Publication>();
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        JsonNode root = mapper.readTree(results);
+
+        JsonNode numFound = root.findValue("hitCount");
+
+        if ( numFound != null && Integer.valueOf(numFound.toString()) > 0 )
+        {
+            JsonNode docs = root.findValue("result");
+
+            Iterator<JsonNode> jsonNodeIterator = docs.elements();
+
+            while ( jsonNodeIterator.hasNext() )
+            {
+                JsonNode doc = jsonNodeIterator.next();
+
+                if ( doc != null )
+                {
+                    System.out.println(doc.toString());
+
+                    Publication gene = mapper.readValue(doc.toString(), Publication.class);
+
+                    publications.add(gene);
+                }
+
+            }
+
+        }
+
+
+        for (Publication pub:publications)
+        {
+            System.out.println(pub.toString());
+            assertTrue(pub.getAuthorString().contains("Gershkovich"));
+        }
+
+    }
+
 
 }
